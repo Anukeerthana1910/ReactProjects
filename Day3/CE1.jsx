acbd771a6f9026f06e9930303b77dfcc4e8e6e97
Anukeerthana1910 <150768210+Anukeerthana1910@users.noreply.github.com>
2024-09-01 14:17:24 +0530

    Update CE1.jsx

--- Day3/CE1.jsx
+++ Day3/CE1.jsx
@@ -1,1 +1,20 @@
+import React, { useState } from 'react';
+
+const ToggleMessage = () => {
+  const [showComponent, setShowComponent] = useState(false);
+  const toggleComponent = () => {
+    setShowComponent(prevState => !prevState);
+  };
+
+  return (
+    <div>
+      <button onClick={toggleComponent}>
+        {showComponent ? 'Hide Component' : 'Show Component'}
+      </button>
+      {showComponent && <p>Hi! How Are You!!!</p>}
+    </div>
+  );
+};
+
+export default ToggleMessage;
 
